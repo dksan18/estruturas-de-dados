e0338e0f251dc30c695399d6311d479a4eba53f1
Tiago Gonzaga <38446221+dksan18@users.noreply.github.com>
2025-02-25 23:47:07 +0000

    alteracao 4

--- exercise/doubly_linked_list.cpp
+++ exercise/doubly_linked_list.cpp
@@ -7,7 +7,7 @@ int main() {
     myList.push_back(10);
     myList.push_back(20);
     myList.push_back(30);
-    std::cout << "Lista após push_back: ";
+    std::cout << "Lista após push_back: ">;
     myList.print();
 
     myList.push_front(5);
